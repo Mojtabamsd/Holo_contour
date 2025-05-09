fe218a4c05e5205aec074a8cfc0d84924c75b16f
mojmas <masoudi.m1991@gmail.com>
2025-05-09 16:58:11 +0100

    add main

---
+++ main.py
@@ -0,0 +1,14 @@
+from particleSizer import particle_sizer
+
+
+particle_sizer(
+    code_dir='.',  # not strictly used in Python version
+    data_dir=r'D:\mojmas\files\Projects\Holo_contour\Matlab\data',
+    im_format='.png',
+    save_dir=r'D:\mojmas\files\Projects\Holo_contour',
+    method_key='structuredForest',  # or 'cannyEdge', 'sobelEdge'
+    model_path=r'D:\mojmas\files\Projects\Holo_contour\model.yml',
+    # name_props=['Area', 'BoundingBox', 'ConvexArea', 'MajorAxisLength', 'MinorAxisLength'],
+    name_props=['Area', 'BoundingBox'],
+    use_convex_hull=True
+)
