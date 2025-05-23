90006b4a7ac7be853b4e5feae5e3961e9abbf537
mojmas <masoudi.m1991@gmail.com>
2025-05-23 16:07:32 +0100

    enable parsing of Markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,11 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'holocontour'
-copyright = '2025, Mojtaba Masoudi'
 author = 'Mojtaba Masoudi'
+copyright = '2025, Mojtaba Masoudi'
 release = '0.1.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "myst_parser",
@@ -23,13 +13,15 @@ extensions = [
     "sphinx.ext.napoleon",
 ]
 
+# Enable parsing of Markdown
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML output options
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
